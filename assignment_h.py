1ca38d07d2aa5cd77abc4dede50d2d7f7c9d3a05
abhilash <124899375+abhilashanilkumar4@users.noreply.github.com>
2023-05-02 23:22:44 +0530

    Add files via upload

---
+++ assignment_h.py
@@ -0,0 +1,22 @@
+def evaluate(expression):
+    stack = []
+    
+    for char in expression:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            if char == "+":
+                result = operand1 + operand2
+            elif char == "-":
+                result = operand1 - operand2
+            elif char == "*":
+                result = operand1 * operand2
+            elif char == "/":
+                result = operand1 / operand2
+            stack.append(result)
+    return stack.pop()
+
+expression = "13*28*+3-"
+print(evaluate(expression))
